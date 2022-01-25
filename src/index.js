3a2ddc09ab1ade41854f7eb6d8c7f13f34413c54
Jageshfartiyal <jageshfartiyal9720@gmail.com>
2022-01-25 16:52:01 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+function Blocks(props) {
+  return (
+    <div className='comma'>
+      <img className="image" src={props.image} alt='image not found'/>
+      <div className="para">
+       
+        <h4><b />{props.head}</h4>
+        <p className="des">{props.description}</p>
+        <a className="anch" href={props.link}>Click Here</a>
+      </div>
+    </div>
+
+  );
+}
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <>
+    <div className="container">
+      <Blocks  image="http://cdn.wallpaperhi.com/2560x1600/20120302/flowers%20orange%20macro%20roses%202560x1600%20wallpaper_www.wallpaperhi.com_21.jpg"
+        head="Category"
+        description="Lorem Ipsum dolor sit amet, consectetuer adipiscingelit.
+        Duis tellus. Donec ante dolor, iaculis nec,"
+        link="#"
+      />
+      <Blocks image="https://tse2.mm.bing.net/th?id=OIP.hc1BYiakuG8bMWkfXnMXwwHaEo&pid=Api&P=0&w=283&h=177"
+        head="Category"
+        description="Lorem Ipsum dolor sit amet, consectetuer adipiscingelit.
+        Duis tellus. Donec ante dolor, iaculis nec,"
+        link="#"
+      />
+      <Blocks image="https://tse1.mm.bing.net/th?id=OIP.MBZCHIkz4xONzP_MDRIJzgHaEo&pid=Api&P=0&w=275&h=171"
+        head="Category"
+        description="Lorem Ipsum dolor sit amet, consectetuer adipiscingelit.
+        Duis tellus. Donec ante dolor, iaculis nec, "
+        link="#"
+      />
+    </div>
+  </>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
